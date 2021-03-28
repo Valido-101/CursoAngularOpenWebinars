bc3cd601268893d0fbee15fc1c9d2bf0548ecd68
Jesús Valido Zafra <jesuszafra22@gmail.com>
2021-03-28 20:17:10 +0200

    Segunda leccion completada

--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/entrada';
 
 @Component({
   selector: 'app-listado',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
+  public listadoEntradas: Entrada[];
 
-  constructor() { }
+  constructor() {
+
+    this.listadoEntradas = [
+      {
+        titulo: 'Introducción a Angular',
+        resumen: 'En esta lección realizaremos una pequeña introducción al mundo de desarrollo con Angular'
+      },
+      {
+        titulo: 'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje de Typescript como base para desarrollar en Angular'
+      },
+      {
+        titulo: 'Componentes en Angular',
+        resumen: 'Aprenderemos a usar los componentes en Angular y el porqué de su importancia'
+      }
+    ];
+
+   }
 
   ngOnInit(): void {
+  }
+
+  public mostrarTitulo(titulo: string): void {
+    alert("Entrada seleccionada: "+ titulo);
   }
 
 }
